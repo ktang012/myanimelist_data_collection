707a08f56582bf0bbf0d4ba7fb88256fe055b6bc
kevin tang <ktang012@ucr.edu>
2019-08-21 22:15:13 -0700

    add conversion to features

--- keras_utils.py
+++ keras_utils.py
@@ -44,22 +44,7 @@ def get_embeddings(vocab, using_gpu=True):
         return cupy.asnumpy(vocab.vectors.data)
     else:
         return vocab.vectors.data
-    
-def get_features(texts, max_length, nlp, batch_size=64):
-    X = np.zeros((len(texts), max_length), dtype="int32")
-    i = 0
-    for doc in nlp.pipe(texts, batch_size=batch_size, disable=["parser", "tagger", "ner"]):
-        for j, token in enumerate(doc):
-            if j >= max_length:
-                break
-            vector_id = token.vocab.vectors.find(key=token.orth)
-            if vector_id >= 0:
-                X[i, j] = vector_id
-            else:
-                X[i, j] = 0
-        i += 1
-    return X
-    
+
 def encode_labels(labels, one_hot=True):
     labels = labels.reset_index(drop=True)
     categories = list(labels.unique())
@@ -72,7 +57,45 @@ def encode_labels(labels, one_hot=True):
         return to_categorical(Y, num_classes=len(categories))
     else:
         return Y
+  
+def get_features_from_sentences(sentences, max_length=300):
+    X = np.zeros((len(sentences), max_length), dtype="int32")
+    i = 0
+    for sent in sentences:
+        for j, token in enumerate(sent):
+            if j >= max_length:
+                break
+            vector_id = token.vocab.vectors.find(key=token.orth)
+            if vector_id >= 0:
+                X[i, j] = vector_id
+            else:
+                X[i, j] = 0
+        i += 1
+    return X
+    
+def get_labelled_sentences_from_doc(doc, doc_label, min_length=15):
+    labels = []
+    sentences = []
+    for sent in doc.sents:
+        if len(sent) >= min_length:
+            sentences.append(sent)
+            labels.append(doc_label)
+    
+    return sentences, np.asarray(labels, dtype="int32")
+
+def encode_text_and_labels(nlp, text, labels, batch_size=64):
+    Xs = []
+    Ys = []
+    encoded_labels = encode_labels(labels)
+    for i, doc in enumerate(nlp.pipe(sample_texts, batch_size=1, disable=["parser", "tagger", "ner"])):
+        sentences, y = get_labelled_sentences_from_doc(doc, encoded_labels[i])
         
+        # TODO: record sentence or token statistics here
+        
+        Xs.append(get_features_from_sentences(sentences))
+        Ys.append(y)
+    return np.vstack(Xs), np.vstack(Ys)
+      
 def group_sentences(document, num_sentences=4):
     sentence_count = 0
     for token in document:
